57a5edc277aef848b53db2d2cd92761f851e26a6
Rithikaa Redde <141546241+rithikaaredde@users.noreply.github.com>
2025-03-06 14:58:12 +0530

    added code

--- Rectangle.java
+++ Rectangle.java
@@ -1,2 +1,26 @@
-public class Rectangle {
+// Rectangle.java
+class Rectangle extends Shape
+{
+    double length, width;
+
+    public Rectangle(double length, double width)
+    {
+        super("Rectangle");
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    void calculateArea()
+    {
+        double area = length * width;
+        System.out.println("Area of Rectangle: " + area);
+    }
+
+    @Override
+    void calculatePerimeter()
+    {
+        double perimeter = 2 * (length + width);
+        System.out.println("Perimeter of Rectangle: " + perimeter);
+    }
 }
