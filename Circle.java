70cd9901cdfc80ebe5e706ad73668c40df66008d
Rithikaa Redde <141546241+rithikaaredde@users.noreply.github.com>
2025-03-06 14:57:30 +0530

    added code

--- Circle.java
+++ Circle.java
@@ -1,2 +1,25 @@
-public class Circle {
+// Circle.java
+class Circle extends Shape 
+{
+    double radius;
+
+    public Circle(double radius)
+    {
+        super("Circle");
+        this.radius = radius;
+    }
+
+    @Override
+    void calculateArea()
+    {
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of Circle: " + area);
+    }
+
+    @Override
+    void calculatePerimeter() 
+    {
+        double perimeter = 2 * Math.PI * radius;
+        System.out.println("Perimeter of Circle: " + perimeter);
+    }
 }
