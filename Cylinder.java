38d67ac2b215316cba0ab0259723d5702bc648d0
Rithikaa Redde <141546241+rithikaaredde@users.noreply.github.com>
2025-03-06 15:00:34 +0530

    added code

--- Cylinder.java
+++ Cylinder.java
@@ -1,2 +1,32 @@
-public class Cylinder {
+// Cylinder.java
+class Cylinder extends Shape implements Volume
+{
+    double radius, height;
+
+    public Cylinder(double radius, double height)
+    {
+        super("Cylinder");
+        this.radius = radius;
+        this.height = height;
+    }
+
+    @Override
+    void calculateArea()
+    {
+        double surfaceArea = 2 * Math.PI * radius * (radius + height);
+        System.out.println("Surface Area of Cylinder: " + surfaceArea);
+    }
+
+    @Override
+    void calculatePerimeter()
+    {
+        System.out.println("Cylinder does not have a perimeter.");
+    }
+
+    @Override
+    public void calculateVolume()
+    {
+        double volume = Math.PI * radius * radius * height;
+        System.out.println("Volume of Cylinder: " + volume);
+    }
 }
