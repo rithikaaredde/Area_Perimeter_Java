fbd6ba94f3af0178836d6ffaccc6fab060bc83ea
Rithikaa Redde <141546241+rithikaaredde@users.noreply.github.com>
2025-03-06 14:58:57 +0530

    added code

--- Square.java
+++ Square.java
@@ -1,2 +1,25 @@
-public class Square {
+//Square.java
+class Square extends Shape
+{
+    double side;
+
+    public Square(double side) 
+    {
+        super("Square");
+        this.side = side;
+    }
+
+    @Override
+    void calculateArea() 
+    {
+        double area = side * side;
+        System.out.println("Area of Square: " + area);
+    }
+
+    @Override
+    void calculatePerimeter() 
+    {
+        double perimeter = 4 * side;
+        System.out.println("Perimeter of Square: " + perimeter);
+    }
 }
